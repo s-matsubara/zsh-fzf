b9b3e448bd835fb86a0c177892741f13e3a9b7ef
Suguru Matsubara <porto.messias@icloud.com>
2023-08-27 08:56:46 +0900

    Removed .idea/workspace.xml and updated cloud-cli/cloud-cl.zsh

--- cloud-cli/cloud-cli.zsh
+++ cloud-cli/cloud-cli.zsh
@@ -1,4 +1,3 @@
 source "$(brew --prefix)/share/cloud-cli/gcp.zsh"
 source "$(brew --prefix)/share/cloud-cli/azure.zsh"
 source "$(brew --prefix)/share/cloud-cli/aws.zsh"
-source "$(brew --prefix)/share/cloud-cli/k8s.zsh"
