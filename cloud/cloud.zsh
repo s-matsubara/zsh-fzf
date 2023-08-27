379feb88ffc6fc317ac269b89893581b9079f21a
Suguru Matsubara <porto.messias@icloud.com>
2023-08-27 09:24:03 +0900

    Updated source paths in cloud.zsh

--- cloud/cloud.zsh
+++ cloud/cloud.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
-source "$(brew --prefix)/share/cloud/gcp.zsh"
-source "$(brew --prefix)/share/cloud/azure.zsh"
-source "$(brew --prefix)/share/cloud/aws.zsh"
+source "$(brew --prefix)/share/zsh-fzf/cloud/gcp.zsh"
+source "$(brew --prefix)/share/zsh-fzf/cloud/azure.zsh"
+source "$(brew --prefix)/share/zsh-fzf/cloud/aws.zsh"
